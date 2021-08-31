298b02fc5fd08591888b951e8ba98898f2c8227b
Jack Rigan <riganjack40@gmail.com>
2021-08-30 19:41:03 -0600

    Implemented axios cancelToken

--- src/App.js
+++ src/App.js
@@ -11,20 +11,29 @@ const [pokemon,setPokemon] = useState([]);
 const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
 const [nextPageUrl, setNextPageUrl] = useState();
 const [prevPageUrl, setPrevPageUrl] = useState();
+const [loading, setLoading] = useState(true);
 
 
 
 useEffect(() => {
-axios.get('https://pokeapi.co/api/v2/pokemon/').then(res => {
-  setPokemon(res.data.results.map(p => p.name))
+  setLoading(true)
+  let cancel;
+  // ? Cancel token  takes an a function and returns the cancel token we need
+axios.get(currentPageUrl, {cancelToken: new axios.CancelToken(c => cancel = c)}).then(res => {
+setLoading(false)  
+setPokemon(res.data.results.map(p => p.name))
   setNextPageUrl(res.data.next);
   setPrevPageUrl(res.data.previous)
   setPokemon(res.data.results.map(p => p.name))
 })
-
 }, [currentPageUrl])
 
 
+
+
+if(loading) {
+  return 'Loading...';
+}
 
   return (
     <div className="App">
