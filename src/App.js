6d873584dade6e82eec9960e57b3d091268ad5ef
Jack Rigan <riganjack40@gmail.com>
2021-08-30 19:34:20 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import PokemonDetail from './components/PokemonDetail';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import PokemonList from './components/PokemonList';
+// import PokemonDetail from './components/PokemonDetail';
+import axios from 'axios';
 
 function App() {
+const [pokemon,setPokemon] = useState([]);
+const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+const [nextPageUrl, setNextPageUrl] = useState();
+const [prevPageUrl, setPrevPageUrl] = useState();
+
+
+
+useEffect(() => {
+axios.get('https://pokeapi.co/api/v2/pokemon/').then(res => {
+  setPokemon(res.data.results.map(p => p.name))
+  setNextPageUrl(res.data.next);
+  setPrevPageUrl(res.data.previous)
+  setPokemon(res.data.results.map(p => p.name))
+})
+
+}, [currentPageUrl])
+
+
+
   return (
     <div className="App">
-<h1>Welcome To Jacks Pokedex</h1>
-<PokemonDetail/>
+<h1>Pokedex</h1>
+<PokemonList pokemon={pokemon}/>
     </div>
   );
 }
--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,26 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React, {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route,Switch,Link, useParams} from 'react-router-dom';
 
 function PokemonDetail() {
+
+
+    const { id } = useParams();
+
 
 const [pokemon, setPokemon] = useState([]);
 
 useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/`)
+    fetch(`https://pokeapi.co/api/v2/pokemon`)
     .then((res) => res.json())
     .then(setPokemon)
     .catch((error) => {
-        console.log(error);
+        console.log(error)
     })
 }, [])
 
-console.log(setPokemon)
+
+// console.log(pokemon)
 
 
     return (
-        <div>
-            
+        <Router>
+        <div className='pokemon-container'>
+            <Switch>
+                <Route exact path='/'>
+           
+                </Route>
+            </Switch>
         </div>
+
+        </Router>
     )
 }
 
